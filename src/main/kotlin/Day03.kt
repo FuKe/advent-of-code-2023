bb374002c3ed74d19c63fa05256c911606866a20
Thomas Sørensen <fuke93@gmail.com>
2023-12-05 21:06:42 +0100

    Progress day 3

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -6,29 +6,68 @@ private fun partOne(puzzleInput: List<String>): Int {
     var sum = 0
     val specialCharRegex = Regex("[^A-Za-z0-9.]")
 
-    puzzleInput.forEach { s ->
-        s.windowed(3, 1, true).forEachIndexed { index, partial ->
-            val lookupIndexEnd = (index + 4).coerceAtMost(s.length-1)
-            val partialWithSurrounding = s.substring((index-1).coerceAtLeast(0), lookupIndexEnd)
 
-            val optNumber: Int? = partial.toIntOrNull()
+    puzzleInput.forEachIndexed { verticalIndex, s ->
+        var lastWholeNumIndexRange: IntRange? = null
 
-            if (optNumber != null) {
-                val relevantLookupLines: List<String> = when (index) {
-                    0 -> listOf(puzzleInput[1])
-                    puzzleInput.size-1 -> listOf(puzzleInput[index-1])
-                    else -> listOf(puzzleInput[index-1], puzzleInput[index+1])
-                }.map {
-                    it.substring(index, lookupIndexEnd)
-                } + partialWithSurrounding
+        s.windowed(5, 1, partialWindows = true).forEachIndexed { horizontalIndex, partial ->
+            val optNumber: Int? = partial.replace(Regex("[^0-9]"), "").toIntOrNull()
 
-                if (relevantLookupLines.any { specialCharRegex.find(it) != null }) {
-                    sum += optNumber
+            if (optNumber != null && optNumber > 10) {
+                val numIndexStart = partial.indexOfFirst { c -> c.isDigit() } + horizontalIndex
+                val numIndexEnd = partial.indexOfLast { c -> c.isDigit() } + horizontalIndex
+
+                val skip = lastWholeNumIndexRange?.contains(numIndexStart) ?: false
+                if (!skip) {
+                    val minLookupIndex = (numIndexStart - 1).coerceAtLeast(0)
+                    val maxLookupIndex = (numIndexEnd + 2).coerceAtMost(s.length - 1)
+
+                    val relevantLookupChunks: List<String> = when (verticalIndex) {
+                        0 -> listOf(puzzleInput[1])
+                        puzzleInput.size-1 -> listOf(puzzleInput[verticalIndex-1])
+                        else -> listOf(puzzleInput[verticalIndex-1], puzzleInput[verticalIndex+1])
+                    }.map {
+                        it.substring(minLookupIndex, maxLookupIndex)
+                    } + partial
+
+                    if (relevantLookupChunks.any { specialCharRegex.find(it) != null }) {
+                        sum += optNumber
+                    }
+
+                    lastWholeNumIndexRange = IntRange(numIndexStart, numIndexEnd)
                 }
             }
-
         }
+
+
+//        s.windowed(5, 1, partialWindows = true).forEachIndexed { horizontalIndex, partial ->
+//            val lookupIndexEnd = (horizontalIndex + 5).coerceAtMost(s.length-1)
+//            val optNumber: Int? = partial.replace(Regex("[^0-9]"), "").toIntOrNull()
+//
+//            if (optNumber != null && optNumber > 10) {
+//                val numIndexStart = partial.indexOfFirst { c -> c.isDigit() } + horizontalIndex
+//                val numIndexEnd = partial.indexOfLast { c -> c.isDigit() } + horizontalIndex
+//
+//                val skip = lastWholeNumIndexRange?.contains(numIndexStart) ?: false
+//                if (!skip) {
+//                    val relevantLookupLines: List<String> = when (verticalIndex) {
+//                        0 -> listOf(puzzleInput[1])
+//                        puzzleInput.size-1 -> listOf(puzzleInput[verticalIndex-1])
+//                        else -> listOf(puzzleInput[verticalIndex-1], puzzleInput[verticalIndex+1])
+//                    }.map {
+//                        it.substring(horizontalIndex, lookupIndexEnd)
+//                    } + partial
+//
+//                    if (relevantLookupLines.any { specialCharRegex.find(it) != null }) {
+//                        sum += optNumber
+//                    }
+//
+//                    lastWholeNumIndexRange = IntRange(numIndexStart, numIndexEnd)
+//                }
+//            }
+//        }
     }
+
 
     return sum
 }
