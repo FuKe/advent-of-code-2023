05fe75892891170c28cbeb73d209d53c2a207aa7
Thomas Vigh Sørensen <tvso@its.aau.dk>
2023-12-11 12:50:45 +0100

    Complete d3p1

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -1,73 +1,40 @@
 fun main() {
+    val puzzleInput: List<String> = readInput("Day03.txt")
+
     testPartOne()
+    val partOneResult: Int = partOne(puzzleInput)
+    println("Part one: $partOneResult")
+
 }
 
 private fun partOne(puzzleInput: List<String>): Int {
     var sum = 0
     val specialCharRegex = Regex("[^A-Za-z0-9.]")
-
+    val numberRegex = Regex("\\d+")
 
     puzzleInput.forEachIndexed { verticalIndex, s ->
-        var lastWholeNumIndexRange: IntRange? = null
+        var optNumberMatch: MatchResult? = numberRegex.find(s)
 
-        s.windowed(5, 1, partialWindows = true).forEachIndexed { horizontalIndex, partial ->
-            val optNumber: Int? = partial.replace(Regex("[^0-9]"), "").toIntOrNull()
+        while (optNumberMatch != null) {
+            val minLookupIndex = (optNumberMatch.range.first - 1).coerceAtLeast(0)
+            val maxLookupIndex = (optNumberMatch.range.last + 2).coerceAtMost(s.length - 1)
 
-            if (optNumber != null && optNumber > 10) {
-                val numIndexStart = partial.indexOfFirst { c -> c.isDigit() } + horizontalIndex
-                val numIndexEnd = partial.indexOfLast { c -> c.isDigit() } + horizontalIndex
+            val relevantLookupChunks: List<String> = when (verticalIndex) {
+                0 -> listOf(puzzleInput[1])
+                puzzleInput.size-1 -> listOf(puzzleInput[verticalIndex-1])
+                else -> listOf(puzzleInput[verticalIndex-1], puzzleInput[verticalIndex+1])
+            }.map {
+                it.substring(minLookupIndex, maxLookupIndex)
+            } + s.substring(minLookupIndex, maxLookupIndex)
 
-                val skip = lastWholeNumIndexRange?.contains(numIndexStart) ?: false
-                if (!skip) {
-                    val minLookupIndex = (numIndexStart - 1).coerceAtLeast(0)
-                    val maxLookupIndex = (numIndexEnd + 2).coerceAtMost(s.length - 1)
-
-                    val relevantLookupChunks: List<String> = when (verticalIndex) {
-                        0 -> listOf(puzzleInput[1])
-                        puzzleInput.size-1 -> listOf(puzzleInput[verticalIndex-1])
-                        else -> listOf(puzzleInput[verticalIndex-1], puzzleInput[verticalIndex+1])
-                    }.map {
-                        it.substring(minLookupIndex, maxLookupIndex)
-                    } + partial
-
-                    if (relevantLookupChunks.any { specialCharRegex.find(it) != null }) {
-                        sum += optNumber
-                    }
-
-                    lastWholeNumIndexRange = IntRange(numIndexStart, numIndexEnd)
-                }
+            if (relevantLookupChunks.any { specialCharRegex.find(it) != null }) {
+                val number: Int = optNumberMatch.value.toInt()
+                println("Adding $number")
+                sum += number
             }
+            optNumberMatch = optNumberMatch.next()
         }
-
-
-//        s.windowed(5, 1, partialWindows = true).forEachIndexed { horizontalIndex, partial ->
-//            val lookupIndexEnd = (horizontalIndex + 5).coerceAtMost(s.length-1)
-//            val optNumber: Int? = partial.replace(Regex("[^0-9]"), "").toIntOrNull()
-//
-//            if (optNumber != null && optNumber > 10) {
-//                val numIndexStart = partial.indexOfFirst { c -> c.isDigit() } + horizontalIndex
-//                val numIndexEnd = partial.indexOfLast { c -> c.isDigit() } + horizontalIndex
-//
-//                val skip = lastWholeNumIndexRange?.contains(numIndexStart) ?: false
-//                if (!skip) {
-//                    val relevantLookupLines: List<String> = when (verticalIndex) {
-//                        0 -> listOf(puzzleInput[1])
-//                        puzzleInput.size-1 -> listOf(puzzleInput[verticalIndex-1])
-//                        else -> listOf(puzzleInput[verticalIndex-1], puzzleInput[verticalIndex+1])
-//                    }.map {
-//                        it.substring(horizontalIndex, lookupIndexEnd)
-//                    } + partial
-//
-//                    if (relevantLookupLines.any { specialCharRegex.find(it) != null }) {
-//                        sum += optNumber
-//                    }
-//
-//                    lastWholeNumIndexRange = IntRange(numIndexStart, numIndexEnd)
-//                }
-//            }
-//        }
     }
-
 
     return sum
 }
